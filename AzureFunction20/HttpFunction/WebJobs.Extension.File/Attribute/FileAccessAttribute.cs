2914a0d9bde5ce12582b791b6a78dac48f68560b
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-10-23 08:59:59 +0530

    Custom Binding ->Attribute and model added

---
+++ AzureFunction20/HttpFunction/WebJobs.Extension.File/Attribute/FileAccessAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.WebJobs.Description;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebJobs.Extension.File.Attribute
+{
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+    [Binding]
+    public class FileAccessAttribute: System.Attribute
+    {
+        [AutoResolve]
+        public string FileName { get; set; }
+        [AppSetting(Default ="Directory")]
+        public string Folder { get; set; }
+    }
+}
---
+++ AzureFunction20/HttpFunction/WebJobs.Extension.File/Model/FileContent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebJobs.Extension.File
+{
+    public class FileContent
+    {
+        public string FileName { get; set; }
+        public string Content { get; set; }
+        public Guid MessageID { get; }
+        public FileContent() => MessageID = Guid.NewGuid();
+    }
+}
