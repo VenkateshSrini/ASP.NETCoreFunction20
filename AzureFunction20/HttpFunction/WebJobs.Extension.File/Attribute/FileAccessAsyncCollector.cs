3ada17a2c47a1b49776d26db28f01f694b2553f5
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-10-24 09:23:13 +0530

    Added the extension config provider and Async collector

---
+++ AzureFunction20/HttpFunction/WebJobs.Extension.File/Attribute/FileAccessAsyncCollector.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebJobs.Extension.File.Attribute
+{
+    public class FileAccessAsyncCollector : IAsyncCollector<FileContent>
+    {
+        string RootPath { get; }
+        public FileAccessAsyncCollector(string rootDirectory)
+        {
+            RootPath = rootDirectory;
+        }
+        public Task AddAsync(FileContent item, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var fullPath = Path.Combine(RootPath, item.FileName);
+            Directory.CreateDirectory(Path.GetFullPath(RootPath));
+            System.IO.File.AppendAllText(fullPath, $"{item.MessageID}:{item.Content}");
+            return Task.CompletedTask;
+        }
+
+        public Task FlushAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+
+}
---
+++ AzureFunction20/HttpFunction/WebJobs.Extension.File/Attribute/FileAccessExtension.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host.Config;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WebJobs.Extension.File.Attribute
+{
+    public class FileAccessExtension : IExtensionConfigProvider
+    {
+        // Root path where files are written. 
+        // Used when attribute.Root is blank 
+        // This is an example of extension-global configuration. 
+        // Generally, attributes should be able to override these settings. 
+        // Make sure these settings are Json serialization friendly. 
+        [JsonProperty("Root")]
+        public string Root { get; set; }
+        public void Initialize(ExtensionConfigContext context)
+        {
+            // Register converters. These help convert between the user's parameter type
+            //  and the type specified by the binding rules. 
+
+            // This allows a user to bind to IAsyncCollector<string>, and the sdk
+            // will convert that to IAsyncCollector<SampleItem>
+            context.AddConverter<string, FileContent>(ConvertToItem);
+            // This is useful on input binding. 
+            context.AddConverter<FileContent, string>(ConvertToString);
+
+            // Create 2 binding rules for the Sample attribute.
+            var rule = context.AddBindingRule<FileAccessAttribute>();
+            //On input binding to read from the file
+            rule.BindToInput<FileContent>(BuildItemFromAttr);
+            //on output binding to make call to AddAsync method in IAyncCollector<FileContent>
+            rule.BindToCollector<FileContent>(BuildCollector);
+        }
+        private FileContent ConvertToItem(string arg)
+        {
+            var parts = arg.Split(':');
+            return new FileContent
+            {
+                FileName = parts[0],
+                Content = parts[1]
+            };
+        }
+        private string ConvertToString(FileContent item)
+        {
+            return item.Content;
+        }
+        private string GetRoot(FileAccessAttribute attribute)
+        {
+            var root = attribute.Folder ?? this.Root ?? Path.GetTempPath();
+            return root;
+        }
+        private IAsyncCollector<FileContent> BuildCollector(FileAccessAttribute attribute)
+        {
+            var root = GetRoot(attribute);
+            return new FileAccessAsyncCollector(root);
+        }
+        private FileContent BuildItemFromAttr(FileAccessAttribute attribute)
+        {
+            var root = GetRoot(attribute);
+            var path = Path.Combine(root, attribute.FileName);
+            if (!System.IO.File.Exists(path))
+            {
+                return null;
+            }
+            var contents = System.IO.File.ReadAllText(path);
+            return new FileContent
+            {
+                FileName = attribute.FileName,
+                Content = contents
+            };
+        }
+    }
+}
