e2966fb1b642a1b00cc260ffb6ec8eb1b6f337e8
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-11-17 18:04:42 +0530

    Files added after successful kubectl deployment

--- AzureFunction20/Docker/Docker.cmd
+++ AzureFunction20/Docker/Docker.cmd
@@ -1,5 +1,9 @@
-﻿docker build -t azurefuncdocker .
+﻿REM docker build -t azurefuncdocker .
+docker build -t venkateshsrini3/azurefunctiondocker .
 docker run -p 8080:80 azurefuncdocker
 REM http://localHost:8080
 REM docker rm  <CONTAINER ID>
 REM docker rmi  <IMAGE ID>
+REM kubectl run azurefuncsample1 --image=venkateshsrini3/azurefuncdocker --port=80
+REM kubectl expose deployment azurefuncsample1 --type=LoadBalancer --target-port=80 --port=3001
+REM kubectl autoscale deploy azurefuncsample1 --cpu-percent=20 --max=5 --min=1
